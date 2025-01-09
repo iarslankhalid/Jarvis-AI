172c865ab941510d9b86c5b3dfbea6fe947a9943
iarslankhalid <rj.arslan750@gmail.com>
2025-01-09 23:38:30 +0500

    Deleting the old Next App

--- next-app/src/components/Card.js
+++
@@ -1,9 +0,0 @@
-export default function Card({ title, description }) {
-    return (
-      <div className="p-4 bg-secondary border border-accent rounded">
-        <h2 className="text-highlight text-lg font-semibold">{title}</h2>
-        <p className="text-light">{description}</p>
-      </div>
-    );
-  }
-  
